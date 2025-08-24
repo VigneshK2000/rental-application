59045259c943c822660644d21aae250277a77830
Vignesh Krishnamurthy <2kvignesh@gmail.com>
2025-08-24 12:10:37 -0500

    Finish auth, update Navbar, Sidebar, routing

--- client/src/app/(nondashboard)/layout.tsx
+++ client/src/app/(nondashboard)/layout.tsx
@@ -3,11 +3,33 @@
 import Navbar from "@/components/Navbar";
 import { NAVBAR_HEIGHT } from "@/lib/constants";
 import { useGetAuthUserQuery } from "@/state/api";
-import React from "react";
+import { usePathname, useRouter } from "next/navigation";
+import React, { useEffect, useState } from "react";
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
-  const { data: authUser } = useGetAuthUserQuery();
-  console.log("authUser", authUser);
+  const { data: authUser, isLoading: authLoading } = useGetAuthUserQuery();
+  const router = useRouter();
+  const pathname = usePathname();
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    if (authUser) {
+      const userRole = authUser.userRole?.toLowerCase();
+      if (
+        (userRole === "manager" && pathname.startsWith("/search")) ||
+        (userRole === "tenant" && pathname === "/")
+      ) {
+        router.push("/managers/properties", { scroll: false });
+        {
+          scroll: false;
+        }
+      } else {
+        setIsLoading(false);
+      }
+    }
+  }, [authUser, pathname, router]);
+
+  if (authLoading || isLoading) return <>Loading...</>;
 
   return (
     <div className="h-full w-full">
